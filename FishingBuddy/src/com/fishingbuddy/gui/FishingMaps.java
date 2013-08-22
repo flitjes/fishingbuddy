7562f18bb61b234c6d2a247d1425850c3c96a684
root <root@flitjesdev-GA-970A-D3>
2013-08-22 19:45:37 +0200

    maps in xml

--- FishingBuddy/src/com/fishingbuddy/gui/FishingMaps.java
+++ FishingBuddy/src/com/fishingbuddy/gui/FishingMaps.java
@@ -1,5 +1,7 @@
 package com.fishingbuddy.gui;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -12,20 +14,30 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FishingMaps extends FragmentActivity{
+public class FishingMaps extends Activity{
 	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 	  static final LatLng KIEL = new LatLng(53.551, 9.993);
 	  private SupportMapFragment fragment= null;
-	@Override
+	  
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);		
+	       setContentView(R.layout.activity_fishing_maps);
+	       
+		}
+	  
+	/*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_fishing_maps);
         fragment = new SupportMapFragment();
         getSupportFragmentManager().beginTransaction()
                 .add(android.R.id.content, fragment).commit();
-        
-        /*GoogleMap map = fragment.getMap();
-        
+       
+                
+        GoogleMap map = fragment.getMap();      
+              
         
         Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
                 .title("Hamburg"));
@@ -40,6 +52,6 @@ public class FishingMaps extends FragmentActivity{
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
 
             // Zoom in, animating the camera.
-            map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);*/
-    }
+            map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }*/
 }
